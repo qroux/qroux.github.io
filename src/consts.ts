ca65b6ddac947793c3d072fd3a670d274e552c96
Quentin Roux <quentin@monpetitgazon.com>
2024-10-05 00:42:55 +0200

    feat: site name + instagram link

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hadrien Chau Photographie'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
